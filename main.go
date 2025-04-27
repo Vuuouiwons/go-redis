ccdf7d005c118000a9ff56bb1f3a891cedc4e453
Vuuouiwons <vinconloren@gmail.com>
2025-04-27 17:43:41 +0700

    Inital commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "redis-cache:6379",
+		Password: "password", // No password set
+		DB:       0,          // Use default DB
+		Protocol: 2,          // Connection protocol
+	})
+
+	ctx := context.Background()
+	
+	// add key: test, with value: payload
+	err := client.Set(ctx, "test", "payload", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	// get key: test
+	val, err := client.Get(ctx, "test").Result()
+	if err != nil {
+		panic(err)
+	}
+	// print it to stdout
+	fmt.Println("test", val)
+
+	// remove key: test
+	err = client.Del(ctx, "test").Err()
+	if err != nil {
+		panic(err)
+	}
+
+}
